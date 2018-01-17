b0a11a912496d9e815bca7a0e4bec9f86aa74116
dasidorenko <sysanin9569@gmail.com>
2018-01-17 19:28:48 +0400

    Add models and controller for user

---
+++ WebAD/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAD.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult СhangeName()
+        {
+            return View();
+        }
+
+        public ActionResult СhangeSurname()
+        {
+            return View();
+        }
+
+        public ActionResult СhangeLogin()
+        {
+            return View();
+        }
+
+        public ActionResult AddGroupMailBox()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        public ActionResult AddUsers()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAD/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAD.Models
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string login { get; set; }
+        public string mailbox { get; set; }
+    }
+}
