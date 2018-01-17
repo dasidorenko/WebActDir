15aeb8ee12ad585674ed0c4a618f687033f9a9a5
IvanSaratov <dfeqcn11@gmail.com>
2018-01-17 15:17:10 +0400

    test

--- WebAD/Controllers/HomeController.cs
+++ WebAD/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebAD.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description страница.";
 
             return View();
         }
